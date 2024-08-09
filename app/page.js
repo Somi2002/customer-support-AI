11f32a7a88f8b37c26d8ae9ba78ce4bdb9f96290
Tanvir <Tanvirislamnyc@gmail.com>
2024-08-08 22:31:35 -0400

    text message box

--- app/page.js
+++ app/page.js
@@ -1,15 +1,75 @@
-'use client'
+"use client";
 import Image from "next/image";
 import { useState } from "react";
-import { Box } from "@mui/material";
+import { Box, Stack, TextField, Button } from "@mui/material";
 
 export default function Home() {
-  const [messages, setMessages] = useState({
-    role: 'assistant',
-    content: `Hello! Welcome to Mental Streak's Customer Support. I'm here to assist you with any questions or issues you might have.`
-  })
-  
-  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([
+    {
+      role: "assistant",
+      content: `Hello! Welcome to Mental Streak's Customer Support. I'm here to assist you with any questions or issues you might have.`,
+    },
+  ]);
 
-  return <Box>Hi</Box>
+  const [message, setMessage] = useState("");
+
+  return (
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      flexdirection="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Stack
+        direction="column"
+        width="600px"
+        height="700px"
+        border="1px solid black"
+        p={2}
+        spacing={3}
+      >
+        <Stack
+          direction="column"
+          spacing={2}
+          flexGrow={1}
+          overflow="auto"
+          maxHeight="100%"
+        >
+          {messages.map((message, index) => (
+            <Box
+              key={index}
+              display="flex"
+              justifyContent={
+                message.role === "assistant" ? "flex-start" : "flex-end"
+              }
+            >
+              <Box
+                bgcolor={
+                  message.role === "assistant"
+                    ? "primary.main"
+                    : "secondary.main"
+                }
+                color="white"
+                borderRadius={16}
+                p={3}
+              >
+                {message.content}
+              </Box>
+            </Box>
+          ))}
+        </Stack>
+        <Stack direction="row" spacing={2}>
+          <TextField
+            label="message"
+            fullWidth
+            value="message"
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <Button variant="contained">Send</Button>
+        </Stack>
+      </Stack>
+    </Box>
+  );
 }
